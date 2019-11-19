c8fe20821187599b8c2b47eb93cffd9d1108de68
Yang Seon <ergaster91@gmail.com>
2019-11-19 14:21:49 +0900

    TodoList.vue 기본구조

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <h1>Todos</h1>
+    <TodoList />
   </div>
 </template>
 
 <script>
 import router from "../router";
+import TodoList from "../components/TodoList";
+
 export default {
   name: "home",
-  components: {},
+  components: {
+    TodoList
+  },
   methods: {
     checkLoggedIn() {
       this.$session.start();
-      if (this.$session.has("jwt")) {
+      if (!this.$session.has("jwt")) {
         // 로그인 페이지로 리다이렉트
         router.push("/login");
       }
