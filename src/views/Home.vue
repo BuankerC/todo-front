1e7a44ee15abee2e1f5b8ee0c8289ed90cc0bb09
Yang Seon <ergaster91@gmail.com>
2019-11-19 16:23:57 +0900

    20191119 | todolist

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="home">
-    <TodoList />
+    <TodoInput @createTodo="createTodo" />
+    <TodoList v-bind:todos="todos" />
   </div>
 </template>
 
 <script>
 import router from "../router";
-import TodoList from "../components/TodoList";
+import TodoList from "../components/TodoList.vue";
+import TodoInput from "@/components/TodoInput.vue";
+import jwtDecode from "jwt-decode";
+import axios from "axios";
 
 export default {
   name: "home",
   components: {
-    TodoList
+    TodoList,
+    TodoInput
+  },
+  data: function() {
+    return {
+      todos: []
+    };
   },
   methods: {
     checkLoggedIn() {
@@ -20,10 +30,58 @@ export default {
         // 로그인 페이지로 리다이렉트
         router.push("/login");
       }
+    },
+    getTodos() {
+      this.$session.start();
+      const token = this.$session.get("jwt");
+      const decodedToken = jwtDecode(token);
+      const user_id = decodedToken.user_id;
+
+      const requestHeader = {
+        headers: {
+          Authorization: "JWT " + token
+        }
+      };
+
+      axios
+        .get(`http://localhost:8000/api/v1/users/${user_id}/`, requestHeader)
+        .then(res => {
+          this.todos = res.data.todo_set;
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    createTodo(title) {
+      this.$session.start();
+      const token = this.$session.get("jwt");
+      const decodedToken = jwtDecode(token);
+      const user_id = decodedToken.user_id;
+
+      const requestHeader = {
+        headers: {
+          Authorization: "JWT " + token
+        }
+      };
+
+      const requestForm = new FormData();
+      requestForm.append("user", user_id);
+      requestForm.append("title", title);
+
+      axios
+        .post("http://localhost:8000/api/v1/todos/", requestForm, requestHeader)
+        .then(res => {
+          console.log(res);
+          this.todos.push(res.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
     }
   },
   mounted: function() {
     this.checkLoggedIn();
+    this.getTodos();
   }
 };
 </script>
